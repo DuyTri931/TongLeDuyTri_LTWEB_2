7db030e9ec15bbe96e47b9c304a1bcefb0d3444e
Duytri391 <tritong558@gmail.com>
2026-06-12 08:24:19 +0700

    add project source

---
+++ src/main/java/com/example/TongLeDuyTri_LTWEB_2/TongLeDuyTriLtweb2Application.java
@@ -0,0 +1,13 @@
+package com.example.TongLeDuyTri_LTWEB_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TongLeDuyTriLtweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TongLeDuyTriLtweb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TongLeDuyTri_LTWEB_2/TongLeDuyTriLtweb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TongLeDuyTri_LTWEB_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongLeDuyTriLtweb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
